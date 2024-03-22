fe72d83cb8570004a6969f2373133b4a7554f673
DanGut19 <111399542+DanGut19@users.noreply.github.com>
2024-03-22 14:04:45 -0600

    Actualizacion 4

--- categorias/src/components/ShowCategorias.jsx
+++ categorias/src/components/ShowCategorias.jsx
@@ -19,14 +19,30 @@ const ShowCategorias = () => {
         getCategories();
     });
 
+    const openModal = (op, name, image) => {
+        setId("");
+        setName("");
+        setImage("");
+        setOperation(op);
+
+        if (operation === 1) {
+            setNameModal("Registrar Categoria");
+        } else if (operation === 2) {
+            setNameModal("Editar Categoria");
+            setIdModal(id);
+            setNameModal(name);
+            setImageModal(image);
+        }
+    }
+
     return (
         <div className='App'>
             <div className='container-fluid'>
                 <div className='row mt-3'>
                     <div className='col-md-4 offset-md-4'>
                         <div className='d-grid mx-auto'>
-                            <button className='btn btn-dark' data-bs-toggle="modal" data-bs-target="#modalCategories">
-                                <i className='fa-solid fa-circle-plus' />Añadir
+                            <button onClick={() => openModal(1)} className='btn btn-dark' data-bs-toggle="modal" data-bs-target="#modalCategories">
+                                <i className='fa-solid fa-circle-plus' /> Añadir
                             </button>
                         </div>
                     </div>
@@ -52,6 +68,14 @@ const ShowCategorias = () => {
                                             <td>{categorie.id}</td>
                                             <td>{categorie.name}</td>
                                             <td>{categorie.image}</td>
+                                            <td>
+                                            <button onClick={() => openModal(2, categories.id, categories.name, categories.image )} className='btn btn-warning' data-bs-toggle='modal' data-bs-target='#modalCategories'>
+                                                <i className='fa-solid fa-edit' />
+                                            </button>
+                                            <button onClick={() => deleteProducto(categories.id)} className='btn btn-danger'>
+                                                <i className='fa-solid fa-trash' />
+                                            </button>
+                                        </td>
                                         </tr>
                                     ))
                                 }
@@ -60,7 +84,6 @@ const ShowCategorias = () => {
                     </div>
                 </div>
             </div>
-
             <div id='modalCategories' className='modal face' aria-hidden="true">
                 <div className='modal-dialog'>
                     <div className='modal-content'>
@@ -69,7 +92,27 @@ const ShowCategorias = () => {
                             <button type='button' className='btn-close' data-bs-dismiss="modal" aria-label='close'/>
                         </div>
                         <div className='modal-body'>
-                            
+                            <imput type="hidden" id="id"/>
+                            <div className='imput-group mb-3'>
+                                <span className='input-group-text'><i className='fa-solid fa-gift'/></span>
+                                <imput type="text" id="id" className="form-control" placeholder="id" value={id} onChange={(e) => setId(e.target.value)}/>
+                            </div>
+                            <div className='imput-group mb-3'>
+                                <span className='input-group-text'><i className='fa-solid fa-gift'/></span>
+                                <imput type="text" id="name" className="form-control" placeholder="name" value={name} onChange={(e) => setName(e.target.value)}/>
+                            </div>
+                            <div className='imput-group mb-3'>
+                                <span className='input-group-text'><i className='fa-solid fa-gift'/></span>
+                                <imput type="text" id="image" className="form-control" placeholder="image" value={image} onChange={(e) => setImage(e.target.value)}/>
+                            </div>
+                        </div>
+                        <div className='modal-footer'>
+                            <button className='btn btn-success'>
+                                <i className='fa-solid fa-floppy-disk'/> Guardar
+                            </button>
+                            <button id='btnCerrarModal' className='btn btn-secondary' data-bs-dismiss="modal">
+                                 Cerrar
+                            </button>
                         </div>
                     </div>
                 </div>
